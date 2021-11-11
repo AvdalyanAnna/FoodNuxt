8380de840f2eadbf85d2f89d5d2464cfd66de895
AvdalyanAnna <anna.avdalyan.2017@gmail.com>
2021-11-11 22:30:14 +0400

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~plugins/app-components.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -52,5 +53,10 @@ export default {
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  server: {
+    port: 8000,
+    host: '0.0.0.0',
+  },
 }
---
+++ plugins/app-components.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+
+// Layouts
+// import Auth from "~/components/Layouts/Auth";
+// import Header from "~/components/Layouts/Header";
+//
+//
+// Vue.component("Auth", Auth);
+// Vue.component("Header", Header);
